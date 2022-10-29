a1c4eae5365b9e606448dbc4314ffd791a8ad15c
ahmed7100 <ahmedsalim7100@gmail.com>
2022-10-29 02:57:49 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - printing a-z
+ *
+ * Return:0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
