5293480034d5cef087d48d6325955136293acffd
ahmed7100 <ahmedsalim7100@gmail.com>
2022-10-29 04:11:37 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - prints _putchar and a new line 
+ * main - prints _putchar and a new line
  * Return: Always 0
  */
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -15,6 +15,5 @@ int main (void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
-return (0);
+	return (0);
 }
