394aecea8435bc6eb31155341b92da9a771f8a89
ahmed7100 <ahmedsalim7100@gmail.com>
2022-10-30 16:40:20 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _islower - check the lowerCAse
- * @c: The character to check 
+ *_islower - check the lowerCAse
+ * @c:The character to check
  *
  * Return:0 or 1
  */
@@ -12,5 +12,5 @@ int _islower(int c)
 	{
 		return (1);
 	}
-	return(0);
+	return (0);
 }
