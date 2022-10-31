53bb352cd981f4c4c078b1447de075576eb9a329
ahmed7100 <ahmedsalim7100@gmail.com>
2022-10-30 17:09:43 -0700

    Task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar(char c);
+
+int largest_number(int a, int b, int c);
+void positive_or_negative(int i);
+void print_remaining_days();
+int convert_days(int month, int day);
+
+#endif
