5c86c1c7546e238dae01092978a3de55d74d5c07
ahmed7100 <ahmedsalim7100@gmail.com>
2022-10-30 17:58:39 -0700

    Task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,7 @@ int _putchar(char c);
 
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
-void print_remaining_days();
+void print_remaining_days(int month, int day, int year);
 int convert_days(int month, int day);
 
 #endif
