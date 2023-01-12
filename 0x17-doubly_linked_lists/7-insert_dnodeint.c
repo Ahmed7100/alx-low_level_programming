bd8784b5e8a248b7017e6ed673e802ea7005c3e0
ahmed7100 <ahmedsalim7100@gmail.com>
2023-01-12 15:51:55 -0800

    Doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,34 +9,46 @@
  * @n: value of the new node
  * Return: the address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *ptr, *ptr1;
-	size_t i;
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-	i = 0;
-	ptr1 = *h;
-	ptr = malloc(sizeof(dlistint_t));
-	ptr->n = n;
-	ptr->next = NULL;
-	ptr->prev = NULL;
-	if (ptr == NULL)
-		return (NULL);
-	if (*h == NULL || h == NULL)
-		return NULL;
-	if (idx == 0) {
-		ptr->next = (*h)->next;
-		ptr->prev = NULL;
-		*h = ptr;
-		return (*h);
+	new = NULL;
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
+	{
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
 	}
-	while (i < (idx - 1)) {
-		ptr1 = ptr1->next;
-		i++;
-	}
-	ptr->next = ptr1->next;
-	ptr1->next = ptr;
-	ptr->prev = ptr1;
-	return (*h);
+
+	return (new);
 }
